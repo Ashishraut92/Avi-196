0b0012b28970bec7a2b0060a3113086961c96c13
Ashishraut92 <ashishraut000@gmail.com>
2022-03-15 13:51:57 +0530

    changes in updatedblog

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -60,38 +60,75 @@ const getBlogs=async function(req,res){
     
 }
 
-const updatedBlog = async function (req, res) {
-    const Id = req.params.blogId
-    try {
-        if (Id) {
-            let data = await BlogModel.findById(Id)
-            if (data.isDeleted == false) {
-    let value1 = req.body.bodyupdate
-        let value2 = req.body.title
-          const arr2 = req.body.subcategory
-          const arr1 = req.body.tags
-           data.tags = data.tags.concat(arr1)
-               const result1 = data.tags.filter(b => b != null)
-                console.log(data.tags)
-                data.subcategory = data.subcategory.concat(arr2)
-                const result2 = data.subcategory.filter(b => b != null)
-                console.log(data.subcategory)
-   let data2 = await BlogModel.findOneAndUpdate({ _id: Id }, { title: value2, body: value1, tags: result1, subcategory: result2 }, { new: true })
-                if (data.isPublished == false)
-      data2 = await BlogModel.findOneAndUpdate({ _id: Id }, { isPublished: true, publishedAt: Date.now() }, { new: true })
-                res.status(200).send({ status: true, msg: data2 })
-            }
-            else
-                res.status(404).send({ status: "false", msg: "data is already deleted" })
+// const updatedBlog = async function (req, res) {
+    const updatedBlog = async (request, response) => {
+        try {
+            const id = request.params.blogId;
+            const data = request.body;
+            const fetchData = await BlogModel.findById(id);
+            if (fetchData.isPublished) {
+                
 
+                // return response.status(404).send({
+                //     'status': false,
+                //     'Error: ': 'Blog Not Found !'
+                // });
+            
+            data.publishedAt = new Date();
+            data.isPublished = true
+            const dataRes = await BlogModel.findByIdAndUpdate(id, data, {
+                new: true
+            });
+            return response.status(200).send({
+                status: true,
+                msg: dataRes
+            });
         }
-        else
-            res.status(404).send({ status: "false", msg: "id is not exist" })
+          return response.status(404).send({
+                 status: false,
+                 Error: 'Blog Not Found !'
+             });
+            }
+        catch (error) {
+            return response.status(500).send({
+                Error: error.message
+            });
+        }
+    }
 
-    }
-    catch (err) { res.status(500).send({ message:err.message }) 
-    }
-}
+
+
+//     const Id = req.params.blogId
+//     try {
+//         if (Id) {
+//             let data = await BlogModel.findById(Id)
+//             if (data.isDeleted == false) {
+//     let value1 = req.body.bodyupdate
+//         let value2 = req.body.title
+//           const arr2 = req.body.subcategory
+//           const arr1 = req.body.tags
+//            data.tags = data.tags.concat(arr1)
+//                const result1 = data.tags.filter(b => b != null)
+//                 console.log(data.tags)
+//                 data.subcategory = data.subcategory.concat(arr2)
+//                 const result2 = data.subcategory.filter(b => b != null)
+//                 console.log(data.subcategory)
+//    let data2 = await BlogModel.findOneAndUpdate({ _id: Id }, { title: value2, body: value1, tags: result1, subcategory: result2 }, { new: true })
+//                 if (data.isPublished == false)
+//       data2 = await BlogModel.findOneAndUpdate({ _id: Id }, { isPublished: true, publishedAt: Date.now() }, { new: true })
+//                 res.status(200).send({ status: true, msg: data2 })
+//             }
+//             else
+//                 res.status(404).send({ status: "false", msg: "data is already deleted" })
+
+//         }
+//         else
+//             res.status(404).send({ status: "false", msg: "id is not exist" })
+
+//     }
+//     catch (err) { res.status(500).send({ message:err.message }) 
+//     }
+// }
 
 
 const BlogDeleted = async function (req, res) {
